0cd6f028b3982d9764ab1651e1fbed27a7f4ec6a
unmesh-temkar <unmeshtemkar.1303@gmail.com>
2025-06-03 15:37:21 +0530

    using data provider annotation

--- src/test/java/advancedPayloads/DynamicJsonTest.java
+++ src/test/java/advancedPayloads/DynamicJsonTest.java
@@ -3,20 +3,21 @@ package advancedPayloads;
 import io.restassured.RestAssured;
 import org.example.Payload;
 import org.example.ReusuableMethods;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
 
 public class DynamicJsonTest {
 
-    @Test
-    public void addBook(){
+    @Test(dataProvider = "BooksData")
+    public void addBook(String isbn, String aisle){
         RestAssured.baseURI = "http://216.10.245.166";
 
         String response =
                 given().log().all()
                         .header("Content-Type", "application/json")
-                        .body(Payload.addBookPayload("unm", "123"))
+                        .body(Payload.addBookPayload(isbn, aisle))
                 .when()
                     .post("Library/Addbook.php")
                 .then().log().all()
@@ -24,5 +25,19 @@ public class DynamicJsonTest {
 
         String id = ReusuableMethods.parseJsonResponse(response,"ID");
         System.out.println("id: " + id);
+        System.out.println("-------------------------------------------");
+    }
+
+    @Test(dataProvider = "BooksData")
+    public void deleteBooks(){
+        RestAssured.baseURI = "http://216.10.245.166";
+
+        String response =
+                given().
+    }
+
+    @DataProvider(name = "BooksData")
+    public String[][] getBooksData(){
+        return new String[][]{{"abc","123"},{"efg","456"},{"ijk","789"}};
     }
 }
